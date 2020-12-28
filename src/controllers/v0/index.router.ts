d51b55cc3396d773640f74928a09938b8411e011
suneetsingrana <54491997+suneetsingrana@users.noreply.github.com>
2020-12-28 15:59:08 +0530

    Add files via upload

--- src/controllers/v0/index.router.ts
+++ src/controllers/v0/index.router.ts
@@ -1,1 +1,13 @@
+import { Router, Request, Response } from 'express';
+import { FilterRouter } from './filter/filter.router';
 
+
+const router: Router = Router();
+
+router.use('/img', FilterRouter);
+
+router.get('/', async (req: Request, res: Response) => {    
+    res.send(`V0`);
+});
+
+export const IndexRouter: Router = router;
